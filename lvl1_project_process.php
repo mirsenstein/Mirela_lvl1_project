7abe5e6e84064d84eb0e1e39304ace39d15e1e74
Mirsi <mirela.katrankieva@inveitix.com>
2019-01-26 00:56:12 +0200

    Final version
    Results in php file.
    Changed variables names.

--- lvl1_project_process.php
+++ lvl1_project_process.php
@@ -1,57 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
 <?php
+echo '<link rel="stylesheet" href="style.css">
+	<title>Document</title>
+</head>
 
-$stringArray= ["abc", 
- "def", 
- "ghi"];
+<body>
+	<div id="instructions">
+		<p>Брой промени за поредицата</p>';
+		
 
-
+$input_string = implode('', $_POST);
+print '<p>"' . $input_string . '"</p>';
+$stringArray = explode('-', $input_string);
 $str_zero = $stringArray[0];
 $last_char = strlen($str_zero) - 1;
 $str_glue = $str_zero[$last_char];
 $count = count($stringArray);
 
-for ($i=1; $i < $count; $i++) { 
-	$str = $stringArray[$i];
-	$str_glue = $str_glue . $str[0];
-	$len = strlen($str);
-	if (($i == ($count - 1))&&($len==1)) {
-		$arr_glue[] = $str_glue;
-	}
-	if ($len > 1) {
-		$arr_glue[] = $str_glue;
-		$str_glue = $str[$len-1];
-	} 
+if (!empty($stringArray)){
+	if($count>1){
+		for ($i=1; $i < $count; $i++) { 
+		$str = $stringArray[$i];
+		$str_glue = $str_glue . $str[0];
+		$len = strlen($str);
+		if (($i == ($count - 1))&&($len==1)) {
+			$arr_glue[] = $str_glue;
+		}
+		if ($len > 1) {
+			$arr_glue[] = $str_glue;
+			$str_glue = $str[$len-1];
+		} 
+		}
 
-}
-
-
-echo "<pre>" . print_r($arr_glue,1) . "</pre>";
-
-$flag1 = 0;
-$flag2 = 0;
-$min = 0;
-$last2 = 0;
-for ($j=0; $j < count($arr_glue); $j++) { 
-	$glue_str = $arr_glue[$j];
-	$len = strlen($glue_str);
-	if ($len==2) {
-		if ($glue_str[0]!=$glue_str[1]) {
-			$flag1++;
-		};
-	}elseif ($len > 2){
-		$max = 1;
-		for ($k=0; $k < strlen($glue_str); $k++) { 
-			$new = substr_count($glue_str, $glue_str[$k]);
-			if ($new > $max) {
-				$max = $new;
+		$single_change = 0;
+		$multi_change = 0;
+		$min_changes = 0;
+		$all_multi_change = 0;
+		for ($j=0; $j < count($arr_glue); $j++) { 
+			$glue_str = $arr_glue[$j];
+			$len = strlen($glue_str);
+			if ($len==2) {
+				if ($glue_str[0]!=$glue_str[1]) {
+					$single_change++;
+				};
+			}elseif ($len > 2){
+				$max = 1;
+				for ($k=0; $k < strlen($glue_str); $k++) { 
+					$new = substr_count($glue_str, $glue_str[$k]);
+					if ($new > $max) {
+						$max = $new;
+					};
+				};
+				$multi_change = strlen($glue_str) - $max;
+				$all_multi_change = $all_multi_change + $multi_change;
 			};
 		};
-		$flag2 = strlen($glue_str) - $max;
-		$last2 = $last2 + $flag2;
-	};
-	// echo '<br>' . $arr_glue[$j] . ' - flag1= ' . $flag1 .  ' flag2= ' . $flag2 . ' last2= '. $last2 . '<br>';
-};
-
-// echo $flag1 . ', ' . $last2;
-$min = $flag1 + $last2;
-print $min;
+		$min_changes = $single_change + $all_multi_change;
+		echo '<div id="results">'. $min_changes .'</div>';
+	}else{
+		echo '<div id="results">0</div>';
+	}
+}
+?>
+<form action="index.html" method="">
+			<button name="count" type="submit">Хайде пак!</button>
+		</form>
+	</div>
+</body>
+</html>
